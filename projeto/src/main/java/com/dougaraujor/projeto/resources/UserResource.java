603bb6d607352114a880d17a5994aa1991bebef8
Douglas Araujo <dougaraujor@gmail.com>
2024-04-15 21:39:09 -0300

    User Entity & Resource

---
+++ projeto/src/main/java/com/dougaraujor/projeto/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.dougaraujor.projeto.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dougaraujor.projeto.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "84999999", "13222");
+        return ResponseEntity.ok().body(u);
+    }
+}
