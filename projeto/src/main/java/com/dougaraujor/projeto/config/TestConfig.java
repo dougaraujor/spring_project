aadcfe620c073071acd95136c1dcaf75d0cf4558
Douglas Araujo <dougaraujor@gmail.com>
2024-04-18 22:40:13 -0300

    OrderStatus  Enum!

--- projeto/src/main/java/com/dougaraujor/projeto/config/TestConfig.java
+++ projeto/src/main/java/com/dougaraujor/projeto/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.dougaraujor.projeto.entities.Order;
 import com.dougaraujor.projeto.entities.User;
+import com.dougaraujor.projeto.entities.enums.OrderStatus;
 import com.dougaraujor.projeto.repositories.OrderRepository;
 import com.dougaraujor.projeto.repositories.UserRepository;
 
@@ -31,9 +32,9 @@ public class TestConfig implements CommandLineRunner {
         // User
 
         // Order
-        Order o1 = new Order(null, Instant.parse("2024-04-18T21:23:09Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2024-04-18T21:23:09Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2024-04-18T21:23:09Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2024-04-18T21:23:09Z"), OrderStatus.DELIVERED, u1);
+        Order o2 = new Order(null, Instant.parse("2024-04-18T21:23:09Z"), OrderStatus.PAID, u2);
+        Order o3 = new Order(null, Instant.parse("2024-04-18T21:23:09Z"), OrderStatus.CANCELED, u1);
         // Order
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
--- projeto/src/main/java/com/dougaraujor/projeto/entities/Order.java
+++ projeto/src/main/java/com/dougaraujor/projeto/entities/Order.java
@@ -3,6 +3,7 @@ package com.dougaraujor.projeto.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.dougaraujor.projeto.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -25,8 +26,10 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
-    @JoinColumn(name = "nid_client", nullable = false)
+    @JoinColumn(name = "nid_client")
     private User client;
 
     // Getters & Setters
@@ -46,6 +49,16 @@ public class Order implements Serializable {
         this.moment = moment;
     }
 
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
     public User getClient() {
         return client;
     }
@@ -59,10 +72,11 @@ public class Order implements Serializable {
     Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         super();
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
     // End of Constructors
